a84ba7aff29d5facce5cf29f775a59ddb37ade40
trindadematheus97 <trindadedt3@gmail.com>
2022-08-22 20:53:11 -0300

    Add project files.

---
+++ Curso/Data/ApplicationContext.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Linq;
+using CursoEFCore.Data.Configurations;
+using CursoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace CursoEFCore.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p=>p.AddConsole());
+
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=CursoEFCore;Integrated Security=true",
+                 p=> p.EnableRetryOnFailure(
+                     maxRetryCount: 2, 
+                     maxRetryDelay: TimeSpan.FromSeconds(5), 
+                     errorNumbersToAdd: null).MigrationsHistoryTable("curso_ef_core"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+            MapearPropriedadesEsquecidas(modelBuilder);
+        }
+
+        private void MapearPropriedadesEsquecidas(ModelBuilder modelBuilder)
+        {
+            foreach(var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entity.GetProperties().Where(p=>p.ClrType == typeof(string));
+
+                foreach(var property in properties)
+                {
+                    if(string.IsNullOrEmpty(property.GetColumnType())
+                        && !property.GetMaxLength().HasValue)
+                        {
+                            //property.SetMaxLength(100);
+                            property.SetColumnType("VARCHAR(100)");
+                        }
+                }
+            }
+        }
+    }
+}
